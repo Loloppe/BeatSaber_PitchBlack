cfd0c354975c51c042266a400d01fe6d2f4ae212
Laurent Blais-Côté <LaurentBlaisCote@outlook.com>
2022-11-27 23:28:17 -0500

    init

---
+++ PitchBlack/Patches/GamePatch.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+
+namespace PitchBlack.Patches
+{
+    [HarmonyPatch(typeof(BeatmapData), nameof(BeatmapData.InsertBeatmapEventData))]
+
+	static class SkipEvent
+    {
+		static bool Prefix(ref BeatmapEventData beatmapEventData)
+		{
+			if(Config.Instance.Enabled)
+            {
+				return false;
+			}
+
+			return true;
+		}
+	}
+
+	[HarmonyPatch(typeof(LightSwitchEventEffect), nameof(LightSwitchEventEffect.Start))]
+
+	static class PitchBlack
+	{
+		static bool Prefix()
+		{
+			if (Config.Instance.Enabled)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+
+
+	[HarmonyPatch(typeof(StaticEnvironmentLights), nameof(StaticEnvironmentLights.Awake))]
+
+	static class RemoveStatic
+	{
+		static bool Prefix()
+		{
+			if (Config.Instance.Enabled)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+
+	[HarmonyPatch(typeof(Spectrogram), nameof(Spectrogram.Update))]
+
+	static class RemoveSpectrogram
+	{
+		static bool Prefix()
+		{
+			if (Config.Instance.Enabled)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
